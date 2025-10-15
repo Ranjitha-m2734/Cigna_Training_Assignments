48b893bf69890dbb095b55de89276926b7d26466
Ranjitha-m2734 <m.ranjitha.das27@gmail.com>
2025-10-15 14:57:13 +0530

    Update SQL_Assignment3.sql

--- SQL_Assignment3.sql
+++ SQL_Assignment3.sql
@@ -13,3 +13,16 @@ CREATE TABLE Emps (
   CONSTRAINT fk_customer FOREIGN KEY (Dept_No) REFERENCES Departments (Dept_No)
 );
 
+INSERT INTO DEPT (deptno, deptname, loc) VALUES (10, 'ACCOUNTING', 'NEW YORK');
+INSERT INTO DEPT (deptno, deptname, loc) VALUES (20, 'RESEARCH', 'DALLAS');
+INSERT INTO DEPT (deptno, deptname, loc) VALUES (30, 'SALES', 'CHICAGO');
+INSERT INTO DEPT (deptno, deptname, loc) VALUES (40, 'OPERATIONS', 'BOSTON');
+
+
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1001, 'JOHN', 'MANAGER', 50000, 10);
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1002, 'ALICE', 'CLERK', 25000, 10);
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1003, 'BOB', 'ANALYST', 40000, 20);
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1004, 'CAROL', 'SALESMAN', 30000, 30);
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1005, 'DAVID', 'SALESMAN', 28000, 30);
+INSERT INTO EMP (empno, empname, job, salary, deptno) VALUES (1006, 'EMILY', 'OPERATOR', 35000, 40);
+
