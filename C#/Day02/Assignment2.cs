c2519e23dd7693e216a2a6b376eb61f96c7a9bb4
Ranjitha-m2734 <m.ranjitha.das27@gmail.com>
2025-11-04 14:55:44 +0530

    Create Assignment2.cs

---
+++ C#/Day02/Assignment2.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Assignment1
+{
+     internal class AreaCalculator
+    {
+         static void AreaCal(double r)
+        {
+            double area = Math.PI * r * r;
+            Console.WriteLine($"Area of Circle is : {area}"); 
+        }
+
+        static void AreaCal(double l, double b)
+        {
+            double area = l * b;
+            Console.WriteLine($"Area of Reactangle is : {area}");
+        }
+
+        static void AreaCal(int s)
+        {
+            int area = s * s;
+            Console.WriteLine($"Area of Square is : {area}");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Enter the Radius for area calculation");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            AreaCal(radius);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the Length for area calculation");
+            double length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the Breadth for area calculation");
+            double breadth = Convert.ToDouble(Console.ReadLine());
+            AreaCal(length, breadth);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter the Side value for area calculation");
+            int side = Convert.ToInt32(Console.ReadLine());
+            AreaCal(side);
+            Console.WriteLine();
+        }
+    }
+}
