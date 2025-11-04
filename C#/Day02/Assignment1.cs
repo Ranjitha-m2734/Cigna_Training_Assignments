c3b13b232434d7da7c8280a48337dbd20627e76a
Ranjitha-m2734 <m.ranjitha.das27@gmail.com>
2025-11-04 14:37:16 +0530

    Create Assignment1.cs

---
+++ C#/Day02/Assignment1.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace Assignment1
+{
+     internal class EmailSender
+    {
+        // Method with optional parameters
+         static void SendEmail(
+            string sender,
+            string recipient,
+            string subject = "No Subject",
+            string message = "No Message",
+            bool isImportant = false)
+        {
+            Console.WriteLine("---- Email Details ----");
+            Console.WriteLine($"From: {sender}");
+            Console.WriteLine($"To: {recipient}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine($"Important: {(isImportant ? "Yes" : "No")}");
+            Console.WriteLine("------------------------\n");
+        }
+
+        static void Main()
+        {
+           
+            SendEmail("scott@gmail.com", "brittni@gmail.com", "Meet and Greet Invite", "Hi" + "\n" + "Hope you are doing well! ", true);
+
+            SendEmail("smith@gmail.com", "team@company.com");
+        }
+    }
+}
