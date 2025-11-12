958fce175f02b234e35b966db169bcfdad33909e
Ranjitha-m2734 <m.ranjitha.das27@gmail.com>
2025-11-12 09:24:14 +0530

    Create Models.cs

---
+++ ASP.NET/Day01/Models.cs
@@ -0,0 +1,12 @@
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Qty { get; set; }
+
+    }
+}
